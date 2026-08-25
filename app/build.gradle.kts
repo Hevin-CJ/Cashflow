856ab64b7715f625ba3fbdca515eba3f2010509e
Hevin C John <hevinchristy@gmail.com>
2026-08-25 15:20:16 +0530

    Several fixes and minor improvements
    Fixed no apps to open csv when open is clicked issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.hevincj.cashflow"
         minSdk = 29
         targetSdk = 36
-        versionCode = 19
-        versionName = "1.1.9"
+        versionCode = 20
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
